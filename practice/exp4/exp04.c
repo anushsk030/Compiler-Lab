40c9c31d855e0848c48faf20cf1040b1a74053c8
ANUSH S KUMAR <145442617+anushsk030@users.noreply.github.com>
2025-10-14 00:27:11 +0530

    Update exp04.c

--- practice/exp4/exp04.c
+++ practice/exp4/exp04.c
@@ -1,22 +1,41 @@
 #include<stdio.h>
-void main()
-{
-    int n;
-    printf("Enter the number of states: ");
-    scanf("%d",&n);
-    if(n==1){
-        printf("Epsilon closure of q0 = { q0,q1,q2 }");
-    }else if(n==2){
-        printf("Epsilon closure of q0 = { q0,q1,q2 }\n");
-        printf("Epsilon closure of q1 = { q1 ,q2}");
-    }else if(n==3){
-        printf("Epsilon closure of q0 = { q0,q1,q2 }\n");
-        printf("Epsilon closure of q1 = { q1 ,q2}\n");
-        printf("Epsilon closure of q2 = { q2 }");
-    }else{
-        printf("Invalid input"); 
+int arr[20][20];
+int res[20];
+int n;
+
+void findEpsilon(int i){
+    res[i] = 1;
+    for(int j = 0 ; j < n ; j++){
+        if(arr[i][j] == 1){
+            findEpsilon(j);
+        }
     }
-    
-    
 }
 
+void main(){
+
+  printf("Enter the no. of states :");
+  scanf("%d",&n);
+  FILE *fp = fopen("input.txt","r");
+  while(!feof(fp)){
+    int state1 , transition , state2;
+    fscanf(fp , "%d%d%d",&state1 , &transition , &state2);
+    if(transition == 404){
+        arr[state1][state2] = 1;
+    }
+  }
+
+  for(int i = 0 ; i < n ; i++){
+    for (int j = 0; j < n; j++) {
+        res[j] = 0;  // or any "default" value
+    }
+    findEpsilon(i);
+    printf("E(%d) = {", i);
+    for(int j = 0 ; j < n ; j++){
+        if(res[j] == 1){
+            printf("%d ,",j);
+        }
+    }
+    printf("}\n");
+  }
+}
